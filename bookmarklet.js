2bea98363f786ce887485cb07043c125c9b856de
Sweepy <git@sweepy.dev>
2026-02-07 16:42:27 -0500

    fix: open in new tab

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,8 @@ javascript: (function () {
   if (window.location.hostname === "github.com" && path.length >= 3) {
     const owner = path[1];
     const repo = path[2];
-    window.location.href = `https://deepwiki.com/${owner}/${repo}`;
+    const url = `https://deepwiki.com/${owner}/${repo}`;
+    window.open(url, "_blank", "noopener,noreferrer");
   } else {
     alert("Please use this bookmarklet on a GitHub repository page.");
   }
